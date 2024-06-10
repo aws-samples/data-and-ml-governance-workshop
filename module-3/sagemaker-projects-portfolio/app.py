03e1a85d01d74515e0c882d3f81f3f64f3f6b92b
Alessandro Cere <alecere@amazon.com>
2024-06-10 15:44:50 +0800

    Added CdkNag to the sagemaker-project-portfolio stack

--- module-3/sagemaker-projects-portfolio/app.py
+++ module-3/sagemaker-projects-portfolio/app.py
@@ -2,14 +2,20 @@
 import os
 
 import aws_cdk as cdk
+from aws_cdk import Aspects
+from cdk_nag import AwsSolutionsChecks, NagSuppressions
 from cdk_pipelines.cdk_pipelines import CdkPipelineStack
 
 app = cdk.App()
-CdkPipelineStack(app, "SmProjectsServiceCatalogPipeline",
+pipeline_stack = CdkPipelineStack(
+    app,
+    "SmProjectsServiceCatalogPipeline",
     description="CI/CD CDK Pipelines for Sagemaker Projects Service Catalog",
     env=cdk.Environment(
         account=os.environ["CDK_DEFAULT_ACCOUNT"],
-        region=os.environ["CDK_DEFAULT_REGION"])
-    )
+        region=os.environ["CDK_DEFAULT_REGION"],
+    ),
+)
+Aspects.of(app).add(AwsSolutionsChecks())
 
 app.synth()
